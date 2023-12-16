3de929e82fc9e2f38bc019023dcd18abb062cb16
sakuya1987 <sakuya1987.0713@icloud.com>
2023-12-16 11:04:53 +0900

    パスワードは英数混合で6文字以上のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}+\z/ }
   validates :last_name,  presence: true, format: { with: /\A[一-龥]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[一-龥]+\z/ }
   validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }

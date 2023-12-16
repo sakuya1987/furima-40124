d00bd5c9c6d582c1871b52ecfe02196be94861fb
sakuya1987 <sakuya1987.0713@icloud.com>
2023-12-16 15:55:16 +0900

    お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}+\z/ }
-  validates :last_name,  presence: true, format: { with: /\A[一-龥]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[一-龥]+\z/ }
+  validates :last_name,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true

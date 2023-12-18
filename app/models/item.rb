890b9fb3ad2400c6645fe551463650a4ff4c5553
sakuya1987 <sakuya1987.0713@icloud.com>
2023-12-18 18:10:49 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :fee_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_id, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end

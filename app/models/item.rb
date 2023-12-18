b6323644202c8bbcefa1db5ba1d0e289a126dff6
sakuya1987 <sakuya1987.0713@icloud.com>
2023-12-18 18:17:42 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,6 @@ class Item < ApplicationRecord
   validates :delivery_id, presence: true
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  belongs_to :user
 end

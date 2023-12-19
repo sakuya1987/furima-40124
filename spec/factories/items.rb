175ee785168ac1918c483c904ba4a8f330784c73
sakuya1987 <sakuya1987.0713@icloud.com>
2023-12-19 22:51:57 +0900

    FactoryBotでitemのダミーデータが作れる

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Name.unique.name }
+    info { Faker::Lorem.sentence }
+    category { Category.where.not(id: 1).sample }
+    status { Status.where.not(id: 1).sample }
+    fee_status { FeeStatus.where.not(id: 1).sample }
+    prefecture { Prefecture.where.not(id: 1).sample }
+    delivery { Delivery.where.not(id: 1).sample }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end

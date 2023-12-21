e7251e5fa27ee74f1612b5c911cc1870173a1e47
sakuya1987 <sakuya1987.0713@icloud.com>
2023-12-21 22:57:21 +0900

    updateアクションをコントローラーに定義する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,16 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private

f10cf454490a28ff14244fdd03a468503b2afc4e
sakuya1987 <sakuya1987.0713@icloud.com>
2023-12-22 14:30:21 +0900

    コードレビュー後の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user == @item.user
+    redirect_to new_user_session_path unless current_user == @item.user
   end
 
   def update

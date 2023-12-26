76c2e8650d53af509100b4b998317f0cbd85936e
sakuya1987 <sakuya1987.0713@icloud.com>
2023-12-26 18:11:41 +0900

    Formオブジェクトを用いて、購入時に複数のテーブルへ情報が保存できる

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
@@ -6,10 +8,20 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index, status: :unprocessable_entity
+    end
   end
+
+  private
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building,
-                                          :phone_number).merge(user_id: current_user.id)
+                                          :phone_number).merge(user_id: current_user.id,
+                                                               item_id: params[:item_id])
   end
 end

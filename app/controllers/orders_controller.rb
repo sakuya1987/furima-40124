07f4f8fc754e9c2e79f01bc25c3f4ccf712bc455
sakuya1987 <sakuya1987.0713@icloud.com>
2023-12-27 16:23:06 +0900

    トークンの情報を受け取る

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,6 +29,6 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building,
                                           :phone_number).merge(user_id: current_user.id,
-                                                               item_id: params[:item_id])
+                                                               item_id: params[:item_id],token: params[:token])
   end
 end
